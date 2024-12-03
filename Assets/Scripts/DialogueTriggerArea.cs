eaaffc21ae9d86bf2f3f5f7f3c763e869a9af496
Sania Tazeen <sania.tazeen04@gmail.com>
2024-12-03 14:12:35 +0530

    prototyping

--- Assets/Scripts/DialogueTriggerArea.cs
+++ Assets/Scripts/DialogueTriggerArea.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class DialogueTriggerArea : MonoBehaviour
 {
     public DialogueTrigger dialogueTrigger; // Reference to the DialogueTrigger on the NPC or object.
-
+    private bool hasTriggered = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) // Check if the player enters the trigger
+        if (!hasTriggered && other.CompareTag("Player")) // Check if the player enters the trigger
         {
             dialogueTrigger.TriggerDialogue(); // Trigger the dialogue
+            hasTriggered = true;
         }
     }
 }
